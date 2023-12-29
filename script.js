495952db7261a1ca4a06eaa1603c9be6854beed4
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2023-12-29 19:53:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let movieCards = document.querySelector(".right");
 let text = `<a href="#" class='moviecard'><div class='movieimage' style="background: url('368175f08c3494fbab5bca96d16a83c5.jpg'); background-size: cover; background-position: center;"></div><br><p id="moviename">The Marvels</p></a>`
 
-for (let i = 0; i < 62; i++) {
+for (let i = 0; i < 66; i++) {
     movieCards.innerHTML += text;
 }
 
