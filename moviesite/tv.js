66a2540f0bb7f79b4d681c4096b43df540c60112
Rickmwasofficial <146796160+Rickmwasofficial@users.noreply.github.com>
2024-01-01 18:48:55 +0300

    Update tv.js

--- moviesite/tv.js
+++ moviesite/tv.js
@@ -3,6 +3,9 @@ let today = document.querySelector(".today");
 let onAir = document.querySelector(".air");
 let search = document.querySelector("#searched");
 let pageBtns = document.querySelector(".pages");
+let myAnchor = document.querySelector(".movieCard");
+let show = {};
+
 let counter = 0;
 for (let i = 1; i <= 10; i++) {
     counter++;
@@ -12,9 +15,9 @@ for (let i = 1; i <= 10; i++) {
 
 function generateNewPage(num) {
     console.log(num);
-    const APILINK = `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&api_key=7cff4a67582d51235859ea119d30aa42&page=${num}`;
-    const APIONAIR = `https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=${num}&api_key=7cff4a67582d51235859ea119d30aa42`;
-    const APITODAY = `https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=${num}&api_key=7cff4a67582d51235859ea119d30aa42`;
+    const APILINK = `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=true&language=en-US&sort_by=popularity.desc&api_key=7cff4a67582d51235859ea119d30aa42&page=${num}`;
+    const APIONAIR = `https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=${num}&include_video=true&api_key=7cff4a67582d51235859ea119d30aa42`;
+    const APITODAY = `https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=${num}&include_video=true&api_key=7cff4a67582d51235859ea119d30aa42`;
     movieCards.innerHTML = "";
     onAir.innerHTML = "";
     today.innerHTML = "";
@@ -26,40 +29,67 @@ function generateNewPage(num) {
 function renderSearchResult() {
     console.log("Search button was clicked");
     let query = search.value;
-    const APISEARCH = `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&language=en-US&page=1&api_key=7cff4a67582d51235859ea119d30aa42`;
+    const APISEARCH = `https://api.themoviedb.org/3/search/multi?query=${query}&include_video=false&include_adult=false&language=en-US&page=1&api_key=7cff4a67582d51235859ea119d30aa42`;
     movieCards.innerHTML = "";
     returnMovies(APISEARCH, movieCards);
 }
 
-const APILINK = 'https://api.themoviedb.org/3/tv/popular?language=en-US&api_key=7cff4a67582d51235859ea119d30aa42&page=1';
+const APILINK = 'https://api.themoviedb.org/3/tv/popular?language=en-US&include_video=true&api_key=7cff4a67582d51235859ea119d30aa42&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500/';
-const APIONAIR = 'https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1&api_key=7cff4a67582d51235859ea119d30aa42';
-const APITODAY = 'https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1&api_key=7cff4a67582d51235859ea119d30aa42';
+const APIONAIR = 'https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1&include_video=true&api_key=7cff4a67582d51235859ea119d30aa42';
+const APITODAY = 'https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1&include_video=true&api_key=7cff4a67582d51235859ea119d30aa42';
 
 returnMovies(APILINK, movieCards);
 returnMovies(APIONAIR, onAir);
 returnMovies(APITODAY, today);
 
+//funcrion to render the results of the GET request
 function returnMovies(url, page) {
     fetch(url).then(res => res.json())
     .then(function(data) {
-        let count = 0;
         console.log(data.results);
         data.results.forEach(element => {
-            if (count < 20) {
-                let title = element.name;
-                let title2 = element.title;
-                if (title == undefined) {
-                    let imagesrc = IMG_PATH + element.poster_path;
-                    let text = `<a href="#" class='moviecard'><div class='movieimage' style="background: url('${imagesrc}'); background-size: cover; background-position: center;"></div><br><p id="moviename">${title2}</p></a>`
-                    page.innerHTML += text;
-                } else {
-                    let imagesrc = IMG_PATH + element.poster_path;
-                    let text = `<a href="#" class='moviecard'><div class='movieimage' style="background: url('${imagesrc}'); background-size: cover; background-position: center;"></div><br><p id="moviename">${title}</p></a>`
-                    page.innerHTML += text;
-                }
-                count++;
+            let title = element.title;
+            let title2 = element.name;
+            let rating = element.vote_average;
+            let desc = element.overview;
+            let country = element.origin_country;
+            
+            if (title == undefined && !(document.getElementById('detailsShow'))) {
+                let imagesrc = IMG_PATH + element.poster_path;
+                show[element.id] = [element.name, rating, desc, country, imagesrc];
+                let text = `<a href="details.html" class='moviecard' onclick="showDetails(${element.id});"><div class='movieimage' style="background: url('${imagesrc}'); background-size: cover; background-position: center;"></div><br><p id="moviename">${title2}</p></a>`
+                page.innerHTML += text;
+                
+            } else if (!(document.getElementById('detailsShow'))){
+                let imagesrc = IMG_PATH + element.poster_path;
+                show[element.id] = [element.title, rating, desc, country, imagesrc];
+                let text = `<a href="details.html" class='moviecard' onclick="showDetails(${element.id});"><div class='movieimage' style="background: url('${imagesrc}'); background-size: cover; background-position: center;"></div><br><p id="moviename">${title}</p></a>`
+                page.innerHTML += text;                
             }
         });
     })
+};
+let castNames = [];
+
+function returnCast(url) {
+    fetch(url).then(res => res.json())
+    .then(function(data) {
+        console.log(data.cast)
+        data.cast.forEach(element => {
+            let castName = element.name;
+            console.log(castNames);
+            if (castNames.length < 10) {
+                castNames.push(castName);
+                console.log(castNames);
+            }
+        });
+        localStorage.setItem('cast', JSON.stringify(castNames));
+    });
+}
+function showDetails(movie) {
+    console.log(castNames)
+    let castLink = `https://api.themoviedb.org/3/tv/${movie}/aggregate_credits?language=en-US&api_key=7cff4a67582d51235859ea119d30aa42`;
+    returnCast(castLink)
+    localStorage.setItem('show', JSON.stringify(show[movie]));
 };
